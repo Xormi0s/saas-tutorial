060618ccdcc2e46f3c1d31f904342c6fd373aff2
Jonas Bourguignon <xormios@hotmail.com>
2024-10-10 16:58:48 +0200

    prep deploy

--- src/saastutorial/settings.py
+++ src/saastutorial/settings.py
@@ -25,8 +25,14 @@ SECRET_KEY = 'django-insecure-*vfs7r3zo$gcvdmiw+7%au^7jb*e%0-$2s%%&5#^z&i#t_1$cg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    ".railway.app"
+]
+if DEBUG:
+    ALLOWED_HOSTS += [
+    "127.0.0.1",
+    "localhost"
+]
 
 # Application definition
 
@@ -37,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Custom apps
+    'visits'
 ]
 
 MIDDLEWARE = [
--- src/saastutorial/views.py
+++ src/saastutorial/views.py
@@ -1,13 +1,28 @@
 import pathlib
 from django.http import HttpResponse
 from django.shortcuts import render
+from visits.models import PageVisit
 
 this_dir = pathlib.Path(__file__).resolve().parent
 
-def home_page_view(request, *args, **kwargs):
+def home_view(request, *args, **kwargs):
+    return about_view(request, *args, **kwargs)
+
+
+def about_view(request, *args, **kwargs):
+    qs = PageVisit.objects.all()
+    page_qs = PageVisit.objects.filter(path=request.path)
+    try:
+        percent = (page_qs.count() * 100.0) / qs.count()
+    except:
+        percent = 0
     my_title = "Test page"
     my_context = {
-        "page_title": my_title
+        "page_title": my_title,
+        "page_visits_count": page_qs.count(),
+        "percent": percent,
+        "total_visits_count": qs.count()
     }
     html_template = "home.html"
+    PageVisit.objects.create(path=request.path)
     return render(request, html_template, my_context)
